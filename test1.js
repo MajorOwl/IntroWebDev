94707840830dc0acc5cb8cd0827d5ee3f15f2dd1
MajorOwl <68669557+MajorOwl@users.noreply.github.com>
2020-07-22 23:06:29 -0400

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,10 @@
+document.addEventListener("mousemove", myFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = Math.random();
+
+}
+
+function removeHandler() {
+    document.removeEventListener("mousemove", myFunction);
+}
